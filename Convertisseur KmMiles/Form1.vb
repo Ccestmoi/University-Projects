42bfcb4646a773aa8579ae74fbc7419cf30c9a41
Ccestmoi <102508239+Ccestmoi@users.noreply.github.com>
2023-11-12 14:51:24 +0100

    Add files via upload

---
+++ Convertisseur KmMiles/Form1.vb
@@ -0,0 +1,13 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim km As Single
+        km = TextBox1.Text
+        Dim mile As Single
+        mile = km / 1.852
+        Label3.Text = mile & " miles"
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        Label3.Text = ""
+    End Sub
+End Class
