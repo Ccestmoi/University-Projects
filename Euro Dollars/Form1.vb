e03deac115579dfc931340cbae8867b570389bfc
Ccestmoi <102508239+Ccestmoi@users.noreply.github.com>
2023-11-12 14:53:42 +0100

    Add files via upload

---
+++ Euro Dollars/Form1.vb
@@ -0,0 +1,25 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        If Double.TryParse(TextBox1.Text, Nothing) Then
+            Dim euros As Double
+            euros = TextBox1.Text
+            Dim tauxchange As Double = 1.18
+            Dim dollars As Double = euros * tauxchange
+            TextBox2.Text = dollars
+        Else
+            TextBox2.Text = ""
+        End If
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs) Handles TextBox2.TextChanged
+        If Double.TryParse(TextBox2.Text, Nothing) Then
+            Dim dollars As Double
+            dollars = TextBox2.Text
+            Dim tauxchange As Double = 1.18
+            Dim euros As Double = dollars / tauxchange
+            TextBox1.Text = euros
+        Else
+            TextBox1.Text = ""
+        End If
+    End Sub
+End Class
