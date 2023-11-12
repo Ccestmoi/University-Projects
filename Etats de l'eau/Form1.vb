58c5bc36f35cde91d5e0afc3cd71416a0557358f
Ccestmoi <102508239+Ccestmoi@users.noreply.github.com>
2023-11-12 14:53:05 +0100

    Add files via upload

---
+++ Etats de l'eau/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim temp As Single
+        temp = TextBox1.Text
+        If temp < 0 Then
+            TextBox2.Text = "Glace"
+        Else
+            If temp > 100 Then
+                TextBox2.Text = "Vapeur"
+            Else
+                TextBox2.Text = "Liquide"
+            End If
+        End If
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox2.Text = ""
+    End Sub
+End Class
